5c9c85bc42e2a6527a608583b1d540c2a0fb82ef
dantegituser <dedisainer@gmail.com>
2020-08-06 18:51:03 -0500

    slider listo reminado new arrivals

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    var paginaSlider = 1;
+    var sliderContenido = document.querySelectorAll('.slider-cont .prod-card');
 
     function addEventListeners() {
         var menuItems = document.querySelectorAll('header .navigation ul.menu-left');
         var megaMenu = document.querySelector('header #mega-menu');
         var cerrarMenu = document.querySelector('li.closeMegaMenu');
+        var sliderLeftArrow = document.querySelector('.flechaizquierda');
+        var sliderRightArrow = document.querySelector('.flechaderecha');
+
+        //console.log(sliderContenido);
 
         menuItems.forEach(element => {
             element.addEventListener('mouseover', () => {
@@ -20,6 +26,52 @@ document.addEventListener('DOMContentLoaded', () => {
         cerrarMenu.addEventListener('click', () => {
             menuInvisible();
         });
+        //acciones click
+        sliderLeftArrow.addEventListener('click', () => {
+            avanzarIzquierdaSlider();
+        });
+        sliderRightArrow.addEventListener('click', () => {
+            avanzarDerechaSlider();
+        });
+    }
+
+
+
+    // funciones
+    function avanzarIzquierdaSlider() {
+
+        if (paginaSlider === 1) {
+            return;
+        } else {
+            // avanza izquierda
+            moverIzquierda();
+            paginaSlider = 1;
+        }
+    }
+
+    function avanzarDerechaSlider() {
+
+        if (paginaSlider === 1) {
+            // avanza derecha
+            moverDerecha();
+            paginaSlider = 2;
+        } else {
+            // no avanza
+            return;
+        }
+    }
+
+    function moverDerecha() {
+        sliderContenido.forEach(elemento => {
+            TweenMax.to(elemento, 1, { x: '-890px' });
+        });
+    }
+
+    function moverIzquierda() {
+        sliderContenido.forEach(elemento => {
+            TweenMax.to(elemento, 1, { x: '0px' });
+        });
+
     }
 
     function menuVisible() {
